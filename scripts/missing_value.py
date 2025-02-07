7e7ed191d5780ee88f0a6e3b5788214e74a4dcd9
semir1r <semir.ali2064@gmail.com>
2025-02-07 11:45:23 +0300

    cleaning data and analyzing

---
+++ scripts/missing_value.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def find_missing_values(df):
+    null_counts = df.isnull().sum()
+    missing_value = null_counts
+    percent_of_missing_value = 100 * null_counts / len(df)
+    data_type = df.dtypes
+
+    missing_data_summary = pd.concat([missing_value, percent_of_missing_value, data_type], axis=1)
+    missing_data_summary_table = missing_data_summary.rename(columns={0: "Missing values", 1: "Percent of Total Values", 2: "DataType"})
+    missing_data_summary_table = missing_data_summary_table[missing_data_summary_table.iloc[:, 1] != 0].sort_values('Percent of Total Values', ascending=False).round(1)
+
+    print(f"From {df.shape[1]} columns selected, there are {missing_data_summary_table.shape[0]} columns with missing values.")
+
+    return missing_data_summary_table
